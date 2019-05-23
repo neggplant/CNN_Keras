e43d76b444f1fec146596a27a8f0930a8e50529d
neggplant <1264021636@qq.com>
2019-05-23 16:24:30 +0800

    add LeNet

---
+++ LeNet.py
@@ -0,0 +1,12 @@
+# 最初针对手写数字识别
+
+def LeNet():
+    model = Sequential()
+    model.add(Conv2D(32, (5, 5), strides=(1, 1), input_shape=(28, 28, 1), padding='valid', activation='relu', kernel_initializer='uniform'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(64, (5, 5),strides=(1, 1), padding='valid', activation='relu', kernel_initializer='uniform'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(100, activation='relu'))
+    model.add(Dense(10, activation='softmax'))
+    return model
